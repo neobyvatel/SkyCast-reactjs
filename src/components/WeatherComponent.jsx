d1e91850fcb165c7847d5903e110aeee3322cb01
neobyvatel <skeezygod@gmail.com>
2023-12-05 00:26:27 +0600

    input change

--- src/components/WeatherComponent.jsx
+++ src/components/WeatherComponent.jsx
@@ -122,15 +122,21 @@ const WeatherComponent = () => {
       </div>
 
       <form className="mx-auto flex w-full flex-col gap-5 font-semibold lg:mx-0 lg:w-1/2 lg:flex-row">
-        <input
-          type="text"
-          placeholder="Enter city name"
-          id="cityName"
-          value={cityName}
-          onKeyDown={handleEnterKeyPress}
-          onChange={handleCityNameChange}
-          className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-purple-500 focus:ring-purple-500 dark:border-gray-600 dark:bg-gray-500 dark:text-white dark:placeholder-gray-400 dark:focus:border-purple-500 dark:focus:ring-purple-500"
-        />
+        {/* <input className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-purple-500 focus:ring-purple-500 dark:border-gray-600 dark:bg-gray-500 dark:text-white dark:placeholder-gray-400 dark:focus:border-purple-500 dark:focus:ring-purple-500" /> */}
+        <div className="relative h-10 w-full min-w-[200px]">
+          <input
+            type="text"
+            placeholder=""
+            id="cityName"
+            value={cityName}
+            onKeyDown={handleEnterKeyPress}
+            onChange={handleCityNameChange}
+            className="text-blue-gray-700 disabled:bg-blue-gray-50 placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border-blue-gray-200 peer h-full w-full rounded-[7px] border bg-transparent  px-3 py-2.5 font-sans text-sm font-normal outline outline-0 transition-all placeholder-shown:border focus:border-2 focus:border-purple-500 focus:border-t-transparent focus:outline-0 disabled:border-0 dark:text-white"
+          />
+          <label className="peer-placeholder-shown:text-blue-gray-500  peer-disabled:peer-placeholder-shown:text-blue-gray-500 before:content[' '] after:content[' '] text-blue-gray-400 before:border-blue-gray-200 after:border-blue-gray-200 pointer-events-none absolute -top-1.5 left-0 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-400 transition-all before:pointer-events-none before:mr-1 before:mt-[6.5px] before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-l before:border-t before:transition-all after:pointer-events-none after:ml-1 after:mt-[6.5px] after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-r after:border-t after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-purple-500 peer-focus:before:border-l-2 peer-focus:before:border-t-2 peer-focus:before:!border-purple-500 peer-focus:after:border-r-2 peer-focus:after:border-t-2 peer-focus:after:!border-purple-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent">
+            Enter city name
+          </label>
+        </div>
         <button
           type="button"
           id="btnFetch"
@@ -142,7 +148,7 @@ const WeatherComponent = () => {
       </form>
 
       <div className="mt-5 flex flex-col flex-wrap gap-5 text-white  md:flex-row [&>*]:rounded-lg [&>*]:p-7 [&>*]:font-bold [&>*]:shadow-lg [&>*]:transition-all">
-        <div className="flex-1 bg-fuchsia-800 hover:bg-fuchsia-900 dark:bg-amber-700 dark:hover:bg-amber-800">
+        <div className="flex-1 border border-transparent bg-fuchsia-800 hover:border-neutral-800 hover:bg-fuchsia-900 dark:bg-amber-700 dark:hover:border-neutral-200 dark:hover:bg-amber-800">
           <p id="updateTime" className="text-sm">
             Update time: {weatherData.updateTime}
           </p>
@@ -156,7 +162,7 @@ const WeatherComponent = () => {
             feels like <span id="feelsLike">{weatherData.feelsLike}</span>
           </p>
         </div>
-        <div className="flex-1 bg-purple-800 hover:bg-purple-900 dark:bg-violet-700 dark:hover:bg-violet-800">
+        <div className="flex-1 border border-transparent bg-purple-800 hover:border-neutral-800 hover:bg-purple-900 dark:bg-violet-700 dark:hover:border-neutral-100 dark:hover:bg-violet-800">
           <h1 id="windSpeed" className="text-4xl">
             {weatherData.windSpeed}
           </h1>
@@ -170,7 +176,7 @@ const WeatherComponent = () => {
             humidity: <span id="humidity">{weatherData.humidity}</span>%
           </p>
         </div>
-        <div className="w-full bg-rose-800 hover:bg-rose-900 dark:bg-fuchsia-700 dark:hover:bg-fuchsia-800">
+        <div className="w-full border border-transparent bg-rose-800 hover:border-neutral-800 hover:bg-rose-900 dark:bg-fuchsia-700 dark:hover:border-neutral-200 dark:hover:bg-fuchsia-800">
           <p id="weatherDescription" className="text-4xl">
             {weatherData.weatherDescription}
           </p>
